54a9d28d08eb8c4a0e5d3a3dee18a4cf5ab7507d
Mridul Chauhan <49958194+HypertextAssassin@users.noreply.github.com>
2021-09-02 13:45:57 +0530

    Added Movie List Data Acces

---
+++ NetChill.Backend/NetChill.Backend.DataAccess/Services/MovieListDataAccess.cs
@@ -0,0 +1,41 @@
+﻿using NetChill.Backend.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetChill.Backend.DataAccess.Services
+{
+    public class MovieListDataAccess : IMovieListDataAccess
+    {
+        private readonly NetChillDbContext _netChillDbContext;
+        public MovieListDataAccess()
+        {
+            this._netChillDbContext = new NetChillDbContext();
+        }
+        
+        /// <inheritdoc cref="IMovieListDataAccess.AddMovieToMovieList(Movie, User)"/>
+        public bool AddMovieToMovieList(Movie movie, User user)
+        {
+            try
+            {
+                this._netChillDbContext.MovieLists.Add(new MovieList
+                {
+                    Movie = movie,
+                    User = user,
+                    MovieId = movie.Id,
+                    UserId = user.Id
+
+                });
+                this._netChillDbContext.SaveChanges();
+                return true;
+            }
+            catch (Exception exception) {
+                Console.WriteLine(exception);
+                return false;
+            }
+        }
+    }
+
+}
