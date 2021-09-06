e71b90d3ea8f9c1737f10813f7b849d0f53f7fb7
Mridul Chauhan <49958194+HypertextAssassin@users.noreply.github.com>
2021-09-06 14:50:58 +0530

    Added Movie Controller

---
+++ NetChill.Backend/NetChill.Backend.Presentation/Controllers/MoviesController.cs
@@ -0,0 +1,32 @@
+﻿using NetChill.Backend.BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace NetChill.Backend.Presentation.Controllers
+{
+    public class MoviesController : ApiController
+    {
+        private readonly MovieBusinessLogic _movieBusinessLogic;
+
+        public MoviesController()
+        {
+            this._movieBusinessLogic = new MovieBusinessLogic();
+        }
+
+        [HttpGet]
+        [Route("Movies")]
+        public IHttpActionResult GetAllMovies() {
+            return Ok(new { Message = "All Movies" });
+        }
+
+        [HttpGet]
+        [Route("Movies/{id}")]
+        public IHttpActionResult GetMovie(int id) {
+            return Ok(new { Message = id });
+        }
+    
+    }
+}
